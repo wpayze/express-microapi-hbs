45fa8eff501307d4f6b04ba73df1feca0b8426e5
Wilfredo Paiz <wilfredopaizpaz@gmail.com>
2018-11-02 15:09:48 -0600

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+
+const personas = [
+  {
+    "id" : "1",
+    "nombre" : "Wilfredo",
+    "edad" : "20"
+  },
+  {
+    "id" : "2",
+    "nombre" : "Zabdy",
+    "edad" : "25"
+  },
+  {
+    "id" : "3",
+    "nombre" : "Minimax",
+    "edad" : "30"
+  }
+];
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { personas: personas });
+});
+
+router.post('/addPerson', function(req,res,next) {
+  const person = {
+    id: personas.length + 1,
+    nombre: req.body.nombre,
+    edad: req.body.edad
+  };
+
+  personas.push(person);
+  res.render('index', {agregado: req.body.nombre, agregadoid: person.id, personas: personas});
+
+});
+
+module.exports = router;
